601f110b749e02a7e453ec5ca858620d6969ebad
jamcardinality <jamescarlcardinal@gmail.com>
2021-12-02 17:30:26 +0800

    Adding data types

--- javaScript-course-2021/01-Fundamentals-Part-1/script.js
+++ javaScript-course-2021/01-Fundamentals-Part-1/script.js
@@ -1,3 +1,4 @@
+/*
 // Linking a JavaScript File
 let js = "amazing";
 console.log(40 + 8 + 23 - 10);
@@ -36,3 +37,26 @@ let population = 109000000;
 console.log(country);
 console.log(continent);
 console.log(population);
+
+*/
+
+// Data Types
+let javascriptIsFun = true;
+console.log(javascriptIsFun);
+
+// console.log(typeof true);
+console.log(typeof javascriptIsFun);
+// console.log(typeof 23);
+// console.log(typeof 'Jonas');
+
+javascriptIsFun = "YES!";
+console.log(typeof javascriptIsFun);
+
+let year;
+console.log(year);
+console.log(typeof year);
+
+year = 1991;
+console.log(typeof year);
+
+console.log(typeof null);
